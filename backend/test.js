f03bd01b69d656286609d074ab52c8cd82fcb9df
dave <djtbsdanner@yahoo.com>
2020-08-30 11:59:42 -0500

    some date stuff

--- backend/test.js
+++ backend/test.js
@@ -1,2 +1,13 @@
 console.log(new Date().toISOString())
 console.log(Math.floor(Math.random() * 1000 + 1))
+var t = Date.now();
+for (var i = 0; i < 1000; i++){
+    console.log(Date.now());
+
+
+}
+var x = Date.now() - t;
+
+var date = new Date(x);
+
+console.log(date.toISOString())
